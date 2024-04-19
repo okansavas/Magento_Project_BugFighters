25f25df1d1c75587c0d9b158f30dd408bf7d1525
turuhsen <tubaaldemir1013@gmail.com>
2024-04-19 19:50:22 +0200

    negatif login added

--- src/test/java/StepDefinitions/_702_Login.java
+++ src/test/java/StepDefinitions/_702_Login.java
@@ -1,9 +1,13 @@
 package StepDefinitions;
 
 import Pages.DialogContent;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class _702_Login {
     DialogContent dc=new DialogContent();
@@ -15,6 +19,15 @@ public class _702_Login {
     @And("the user clicks the SignIn button")
     public void theUserClicksTheSignInButton() {
         dc.myClick(dc.signInButton);
+    }
+    @Then("user confirms error message that appears on the screen.")
+    public void userConfirmsErrorMessageThatAppearsOnTheScreen(DataTable message) {
+        List<String> strButoonList=message.asList(String.class);
+        for (int i = 0; i < strButoonList.size(); i++) {
+            WebElement linkWebElemnt=dc.getWebElement(strButoonList.get(i));
+            dc.verifyContainsText(linkWebElemnt,"required");
+
+        }
     }
 
     @Then("user confirms that a successful SignIn process has taken place")
