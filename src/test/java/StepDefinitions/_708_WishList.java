e82de1fa35421223936ad2eda2d3c00b7ac6a32e
ermankaratash <48660429+ermankaratash@users.noreply.github.com>
2024-04-21 13:59:27 +0200

    Scenario has written

---
+++ src/test/java/StepDefinitions/_708_WishList.java
@@ -0,0 +1,44 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _708_WishList {
+    @Given("User clicks the {string} from the header menu")
+    public void userClicksTheFromTheHeaderMenu(String arg0) {
+    }
+
+    @When("User hovers an item from the items")
+    public void userHoversAnItemFromTheItems() {
+    }
+
+    @And("User clicks the heart icon")
+    public void userClicksTheHeartIcon() {
+    }
+
+    @Then("User sees the {string}")
+    public void userSeesThe(String arg0) {
+    }
+
+    @When("User clicks My Account in the dropdown menu under users name")
+    public void userClicksMyAccountInTheDropdownMenuUnderUsersName() {
+    }
+
+    @And("User clicks My Wish List on the left menu")
+    public void userClicksMyWishListOnTheLeftMenu() {
+    }
+
+    @Then("User sees the product that was added to the wish list")
+    public void userSeesTheProductThatWasAddedToTheWishList() {
+    }
+
+    @When("User hovers the item in the wish list")
+    public void userHoversTheItemInTheWishList() {
+    }
+
+    @And("User clicks the trash icon")
+    public void userClicksTheTrashIcon() {
+    }
+}
